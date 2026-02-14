c27559cb816362df65d09d9057b8b4e2ba59c974
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2026-02-14 20:39:49 +0000

    Basic falling animation and game continuous loop

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import { motion } from "framer-motion"
 
 import VocabWord from "@/components/VocabWord";
 import vocabularyRaw from "@/data/genki1_1.json";
@@ -10,43 +11,77 @@ export default function GameBoard() {
     const [displayedWordID, setDisplayedWordID] = useState<number>(0)
     const [userGuess, setUserGuess] = useState<string>("")
     const [falseText, setFalseText] = useState<string>("")
+    const [missTotal, setMissTotal] = useState<number>(0)
+    const [correctTotal, setCorrectTotal] = useState<number>(0)
 
     const vocab: VocabWordType[] = vocabularyRaw.vocabulary;
+
+    //function to reset game
+    function reset() {
+        setDisplayedWordID(0);
+        setUserGuess("");
+        setFalseText("");
+        setMissTotal(0);
+        setCorrectTotal(0);
+    }
+
+    function nextWord() {
+        setDisplayedWordID(displayedWordID + 1);
+    }
+
+    function handleMiss() {
+        setMissTotal(missTotal + 1);
+        nextWord();
+    }
 
     const handleGuess = (e: React.SubmitEvent) => {
         e.preventDefault();
 
-        setFalseText("")
+        setFalseText("");
 
         if (userGuess == vocab[displayedWordID].meaning) {
             setUserGuess("");
+            setCorrectTotal(correctTotal + 1)
             if (displayedWordID >= vocab.length - 1) {
                 setDisplayedWordID(0);
             }
             else {
-                setDisplayedWordID(displayedWordID + 1);
+                nextWord();
             }
         }
         else {
-            setFalseText("Try again!")
+            setFalseText("Try again!");
+            setUserGuess("");
         }
     }
 
     return (
         <div className="flex flex-col content-center items-center bg-yellow-100 h-150 w-5xl ">
-            <div className="flex flex-col gap-10 items-center">
+            <motion.div
+                className="flex-1"
+                key={displayedWordID}
+                initial={{ y: 0 }}
+                animate={{ y: 450 }}
+                transition={{ duration: 10, ease: "linear" }}
+                onAnimationComplete={handleMiss}
+            >
                 <VocabWord word={vocab[displayedWordID]} />
-                <p className="text-red-500">{falseText}</p>
-            </div>
+            </motion.div>
 
-            <form onSubmit={handleGuess}>
-                <input className="bg-white border w-125"
-                    type="text"
-                    name="Guess"
-                    value={userGuess}
-                    onChange={(e) => setUserGuess(e.target.value)}
-                />
-            </form>
+
+            {falseText && <p className="text-red-500">{falseText}</p>}
+            <div className="flex flex row gap-4">
+                {correctTotal > 0 && <p>Correct: {correctTotal}</p>}
+                <form onSubmit={handleGuess}>
+                    <input className="bg-white border w-125 mb-4"
+                        type="text"
+                        name="Guess"
+                        value={userGuess}
+                        onChange={(e) => setUserGuess(e.target.value)}
+                    />
+                </form>
+                {missTotal > 0 && <p>Misses: {missTotal}</p>}
+            </div>
         </div>
     )
 }

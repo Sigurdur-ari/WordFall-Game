7dfb103c28cb9b4e42f61a391b700581265742b1
Sigurður Ari Stefánsson <sigurdur.ari@gmail.com>
2026-03-29 14:10:42 +0000

    Add chapter 3 vocab

--- src/data/genki1/chapter3.ts
+++ src/data/genki1/chapter3.ts
@@ -4,25 +4,449 @@ export const chapter3: VocabWordType[] = [
     {
         id: "1",
         chapter: 3,
-        japanese: "test kana 1",
-        kanji: "test kanji 1",
-        meaning: ["test kana 1"],
+        japanese: "えいが",
+        kanji: "映画",
+        meaning: ["movie"],
         wordType: "noun"
     },
     {
         id: "2",
         chapter: 3,
-        japanese: "test kana 2",
-        kanji: "test kanji 2",
-        meaning: ["test kana 2"],
+        japanese: "おんがく",
+        kanji: "音楽",
+        meaning: ["music"],
         wordType: "noun"
     },
     {
         id: "3",
         chapter: 3,
-        japanese: "test kana 3",
-        kanji: "test kanji 3",
-        meaning: ["test kana 3"],
+        japanese: "ざっし",
+        kanji: "雑誌",
+        meaning: ["magazine", "journal"],
         wordType: "noun"
     },
-]
+    {
+        id: "4",
+        chapter: 3,
+        japanese: "スポーツ",
+        kanji: "",
+        meaning: ["sports", "sport"],
+        wordType: "noun"
+    },
+    {
+        id: "5",
+        chapter: 3,
+        japanese: "デート",
+        kanji: "",
+        meaning: ["date"],
+        wordType: "noun"
+    },
+    {
+        id: "6",
+        chapter: 3,
+        japanese: "テニス",
+        kanji: "",
+        meaning: ["tennis"],
+        wordType: "noun"
+    },
+    {
+        id: "7",
+        chapter: 3,
+        japanese: "テレビ",
+        kanji: "",
+        meaning: ["tv", "television"],
+        wordType: "noun"
+    },
+    {
+        id: "8",
+        chapter: 3,
+        japanese: "アイスクリーム",
+        kanji: "",
+        meaning: ["ice cream"],
+        wordType: "noun"
+    },
+    {
+        id: "9",
+        chapter: 3,
+        japanese: "ハンバーガー",
+        kanji: "",
+        meaning: ["hamburger"],
+        wordType: "noun"
+    },
+    {
+        id: "10",
+        chapter: 3,
+        japanese: "おさけ",
+        kanji: "お酒",
+        meaning: ["sake", "alcohol", "alcoholic drink", "japanese sake"],
+        wordType: "noun"
+    },
+    {
+        id: "11",
+        chapter: 3,
+        japanese: "おちゃ",
+        kanji: "お茶",
+        meaning: ["green tea", "tea"],
+        wordType: "noun"
+    },
+    {
+        id: "12",
+        chapter: 3,
+        japanese: "コーヒー",
+        kanji: "",
+        meaning: ["coffee"],
+        wordType: "noun"
+    },
+    {
+        id: "13",
+        chapter: 3,
+        japanese: "みず",
+        kanji: "水",
+        meaning: ["water"],
+        wordType: "noun"
+    },
+    {
+        id: "14",
+        chapter: 3,
+        japanese: "あさごはん",
+        kanji: "朝ご飯",
+        meaning: ["breakfast"],
+        wordType: "noun"
+    },
+    {
+        id: "15",
+        chapter: 3,
+        japanese: "ひるごはん",
+        kanji: "昼ご飯",
+        meaning: ["lunch"],
+        wordType: "noun"
+    },
+    {
+        id: "16",
+        chapter: 3,
+        japanese: "ばんごはん",
+        kanji: "晩ご飯",
+        meaning: ["dinner"],
+        wordType: "noun"
+    },
+    {
+        id: "17",
+        chapter: 3,
+        japanese: "いえ",
+        kanji: "家",
+        meaning: ["home", "house"],
+        wordType: "noun"
+    },
+    {
+        id: "18",
+        chapter: 3,
+        japanese: "うち",
+        kanji: "家",
+        meaning: ["home", "house", "my house", "my place"],
+        wordType: "noun"
+    },
+    {
+        id: "19",
+        chapter: 3,
+        japanese: "がっこう",
+        kanji: "学校",
+        meaning: ["school"],
+        wordType: "noun"
+    },
+    {
+        id: "20",
+        chapter: 3,
+        japanese: "カフェ",
+        kanji: "",
+        meaning: ["cafe", "coffee shop"],
+        wordType: "noun"
+    },
+    {
+        id: "21",
+        chapter: 3,
+        japanese: "あした",
+        kanji: "明日",
+        meaning: ["tomorrow"],
+        wordType: "noun"
+    },
+    {
+        id: "22",
+        chapter: 3,
+        japanese: "きょう",
+        kanji: "今日",
+        meaning: ["today"],
+        wordType: "noun"
+    },
+    {
+        id: "23",
+        chapter: 3,
+        japanese: "あさ",
+        kanji: "朝",
+        meaning: ["morning"],
+        wordType: "noun"
+    },
+    {
+        id: "24",
+        chapter: 3,
+        japanese: "こんばん",
+        kanji: "今晩",
+        meaning: ["tonight", "this evening"],
+        wordType: "noun"
+    },
+    {
+        id: "25",
+        chapter: 3,
+        japanese: "まいにち",
+        kanji: "毎日",
+        meaning: ["every day", "each day"],
+        wordType: "noun"
+    },
+    {
+        id: "26",
+        chapter: 3,
+        japanese: "まいばん",
+        kanji: "毎晩",
+        meaning: ["every night", "every evening"],
+        wordType: "noun"
+    },
+    {
+        id: "27",
+        chapter: 3,
+        japanese: "しゅうまつ",
+        kanji: "週末",
+        meaning: ["weekend"],
+        wordType: "noun"
+    },
+    {
+        id: "28",
+        chapter: 3,
+        japanese: "どようび",
+        kanji: "土曜日",
+        meaning: ["saturday"],
+        wordType: "noun"
+    },
+    {
+        id: "29",
+        chapter: 3,
+        japanese: "にちようび",
+        kanji: "日曜日",
+        meaning: ["sunday"],
+        wordType: "noun"
+    },
+    {
+        id: "30",
+        chapter: 3,
+        japanese: "いつ",
+        kanji: "",
+        meaning: ["when"],
+        wordType: "other"
+    },
+    {
+        id: "31",
+        chapter: 3,
+        japanese: "〜ごろ",
+        kanji: "",
+        meaning: ["at about", "around"],
+        wordType: "postfix"
+    },
+    {
+        id: "32",
+        chapter: 3,
+        japanese: "いく",
+        kanji: "行く",
+        meaning: ["to go", "go"],
+        wordType: "u- verb"
+    },
+    {
+        id: "33",
+        chapter: 3,
+        japanese: "かえる",
+        kanji: "帰る",
+        meaning: ["to return", "to go back"],
+        wordType: "u- verb"
+    },
+    {
+        id: "34",
+        chapter: 3,
+        japanese: "きく",
+        kanji: "聞く",
+        meaning: ["to listen", "to hear"],
+        wordType: "u- verb"
+    },
+    {
+        id: "35",
+        chapter: 3,
+        japanese: "のむ",
+        kanji: "飲む",
+        meaning: ["to drink"],
+        wordType: "u- verb"
+    },
+    {
+        id: "36",
+        chapter: 3,
+        japanese: "はなす",
+        kanji: "話す",
+        meaning: ["to speak", "to talk"],
+        wordType: "u- verb"
+    },
+    {
+        id: "37",
+        chapter: 3,
+        japanese: "よむ",
+        kanji: "読む",
+        meaning: ["to read"],
+        wordType: "u- verb"
+    },
+    {
+        id: "38",
+        chapter: 3,
+        japanese: "おきる",
+        kanji: "起きる",
+        meaning: ["to get up", "to wake up"],
+        wordType: "ru- verb"
+    },
+    {
+        id: "39",
+        chapter: 3,
+        japanese: "たべる",
+        kanji: "食べる",
+        meaning: ["to eat"],
+        wordType: "ru- verb"
+    },
+    {
+        id: "40",
+        chapter: 3,
+        japanese: "ねる",
+        kanji: "寝る",
+        meaning: ["to sleep", "to go to sleep"],
+        wordType: "ru- verb"
+    },
+    {
+        id: "41",
+        chapter: 3,
+        japanese: "みる",
+        kanji: "見る",
+        meaning: ["to see", "to look at", "to watch"],
+        wordType: "ru- verb"
+    },
+    {
+        id: "42",
+        chapter: 3,
+        japanese: "くる",
+        kanji: "来る",
+        meaning: ["to come"],
+        wordType: "irr- verb"
+    },
+    {
+        id: "43",
+        chapter: 3,
+        japanese: "する",
+        kanji: "",
+        meaning: ["to do"],
+        wordType: "irr- verb"
+    },
+    {
+        id: "44",
+        chapter: 3,
+        japanese: "べんきょうする",
+        kanji: "勉強する",
+        meaning: ["to study"],
+        wordType: "irr- verb"
+    },
+    {
+        id: "45",
+        chapter: 3,
+        japanese: "いい",
+        kanji: "良い",
+        meaning: ["good"],
+        wordType: "adjective"
+    },
+    {
+        id: "46",
+        chapter: 3,
+        japanese: "はやい",
+        kanji: "早い",
+        meaning: ["early"],
+        wordType: "adjective"
+    },
+    {
+        id: "47",
+        chapter: 3,
+        japanese: "あまり + negative",
+        kanji: "",
+        meaning: ["not much"],
+        wordType: "other"
+    },
+    {
+        id: "48",
+        chapter: 3,
+        japanese: "ぜんぜん + negative",
+        kanji: "全然",
+        meaning: ["not at all"],
+        wordType: "other"
+    },
+    {
+        id: "49",
+        chapter: 3,
+        japanese: "たいてい",
+        kanji: "",
+        meaning: ["usually"],
+        wordType: "other"
+    },
+    {
+        id: "50",
+        chapter: 3,
+        japanese: "ちょっと",
+        kanji: "",
+        meaning: ["a little", "a bit"],
+        wordType: "other"
+    },
+    {
+        id: "51",
+        chapter: 3,
+        japanese: "ときどき",
+        kanji: "時々",
+        meaning: ["sometimes"],
+        wordType: "other"
+    },
+    {
+        id: "52",
+        chapter: 3,
+        japanese: "よく",
+        kanji: "",
+        meaning: ["often"],
+        wordType: "other"
+    },
+    {
+        id: "53",
+        chapter: 3,
+        japanese: "そうですね",
+        kanji: "",
+        meaning: ["that's right", "let me see"],
+        wordType: "other"
+    },
+    {
+        id: "54",
+        chapter: 3,
+        japanese: "でも",
+        kanji: "",
+        meaning: ["but"],
+        wordType: "other"
+    },
+    {
+        id: "55",
+        chapter: 3,
+        japanese: "どうですか",
+        kanji: "",
+        meaning: ["how about", "how is it"],
+        wordType: "other"
+    },
+    {
+        id: "56",
+        chapter: 3,
+        japanese: "ええ",
+        kanji: "",
+        meaning: ["yes"],
+        wordType: "other"
+    },
+];
